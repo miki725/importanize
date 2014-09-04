0c79929cddba676872df396d8c8bfcd3f8f4d280
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2014-09-04 19:03:50 -0400

    renamed importanize module to statements

--- importanize/parser.py
+++ importanize/parser.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-from .importanize import CHARS, DOTS, ImportLeaf, ImportStatement
+from .statements import CHARS, DOTS, ImportLeaf, ImportStatement
 from .utils import list_strip
 
 
