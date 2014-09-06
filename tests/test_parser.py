1ff37aaf421c8940ffeef60ef2532d891121a70e
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2014-09-06 16:09:12 -0400

    added test for multiple imports exception

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,6 +4,7 @@ import unittest
 
 import six
 
+from importanize.exceptions import MultipleImportsError
 from importanize.parser import find_imports_from_lines, parse_statements
 
 
@@ -127,6 +128,9 @@ class TestParsing(unittest.TestCase):
             'import ..a.b',
             'from ..a import b'
         )
+
+        with self.assertRaises(MultipleImportsError):
+            list(parse_statements([('import a, b', [1])]))
 
     def test_from_statements(self):
         """
