5b93cdcce34f43228a8eaeae6d99d04721b230b7
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2014-09-05 22:11:30 -0400

    added some tests for ImportStatement

--- tests/test_statements.py
+++ tests/test_statements.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
+import mock
 import six
 import unittest
 
-from importanize.statements import ImportLeaf
+from importanize.statements import ImportLeaf, ImportStatement
 
 
 class TestImportLeaf(unittest.TestCase):
@@ -39,6 +40,10 @@ class TestImportLeaf(unittest.TestCase):
         leaf = ImportLeaf('a as a')
         self.assertEqual(six.text_type(leaf), leaf.as_string())
 
+    def test_eq(self):
+        self.assertTrue(ImportLeaf('a') == ImportLeaf('a'))
+        self.assertFalse(ImportLeaf('a') == ImportLeaf('b'))
+
     def test_gt(self):
         self.assertGreater(
             ImportLeaf('b'),
@@ -65,4 +70,82 @@ class TestImportLeaf(unittest.TestCase):
             repr(ImportLeaf('a')),
             '<{}.{} object - "a">'.format(ImportLeaf.__module__,
                                           ImportLeaf.__name__)
+        )
+
+    def test_hash(self):
+        self.assertEqual(
+            hash(ImportLeaf('a')),
+            hash('a')
+        )
+        self.assertEqual(
+            hash(ImportLeaf('a as b')),
+            hash('a+b')
+        )
+
+
+class TestImportStatement(unittest.TestCase):
+    def test_init(self):
+        actual = ImportStatement(mock.sentinel.line_numbers,
+                                 mock.sentinel.stem)
+        self.assertEqual(actual.line_numbers, mock.sentinel.line_numbers)
+        self.assertEqual(actual.stem, mock.sentinel.stem)
+        self.assertEqual(actual.leafs, [])
+
+        actual = ImportStatement(mock.sentinel.line_numbers,
+                                 mock.sentinel.stem,
+                                 mock.sentinel.leafs)
+        self.assertEqual(actual.line_numbers, mock.sentinel.line_numbers)
+        self.assertEqual(actual.stem, mock.sentinel.stem)
+        self.assertEqual(actual.leafs, mock.sentinel.leafs)
+
+    def test_root_module(self):
+        self.assertEqual(
+            ImportStatement([], 'a').root_module,
+            'a'
+        )
+        self.assertEqual(
+            ImportStatement([], 'a.b.c').root_module,
+            'a'
+        )
+        self.assertEqual(
+            ImportStatement([], '.a').root_module,
+            ''
+        )
+
+    def test_as_string(self):
+        def _test(stem, leafs, expected):
+            statement = ImportStatement(
+                list(),
+                stem,
+                list(map(ImportLeaf, leafs))
+            )
+            self.assertEqual(statement.as_string(), expected)
+
+        _test('a', [], 'import a')
+        _test('a.b.c', [], 'import a.b.c')
+        _test('a.b', ['c'], 'from a.b import c')
+        _test('a.b', ['c', 'd'], 'from a.b import c, d')
+        _test('a.b', ['c as d', 'e'], 'from a.b import c as d, e')
+        _test('a.b', ['e', 'c as d', 'e'], 'from a.b import c as d, e')
+
+    def test_str(self):
+        statement = ImportStatement([], 'a')
+        self.assertEqual(
+            statement.as_string(),
+            six.text_type(statement),
+        )
+
+    def test_eq(self):
+        self.assertTrue(
+            ImportStatement([], 'a', [ImportLeaf('a')])
+            == ImportStatement([], 'a', [ImportLeaf('a')])
+        )
+        self.assertTrue(
+            ImportStatement([], 'a', [ImportLeaf('a')])
+            == ImportStatement([], 'a', [ImportLeaf('a'),
+                                         ImportLeaf('a')])
+        )
+        self.assertFalse(
+            ImportStatement([], 'a', [ImportLeaf('a')])
+            == ImportStatement([], 'a', [ImportLeaf('b')])
         )
